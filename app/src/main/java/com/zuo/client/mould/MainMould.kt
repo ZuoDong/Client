45c9bc3ef73d768c01422a8267d08b076f34cba6
zuodong <zuodong@gamil.com>
2018-03-12 08:07:29 +0800

    creat view and mould

--- app/src/main/java/com/zuo/client/mould/MainMould.kt
+++ app/src/main/java/com/zuo/client/mould/MainMould.kt
@@ -1,5 +1,19 @@
 package com.zuo.client.mould
 
+import android.content.Context
+import com.zuo.client.R
+import com.zuo.client.bean.LastFmBean
+import com.zuo.client.httpService.ClientResponse
+import com.zuo.client.httpService.NetService
+
 /**
  * Created by dongdong on 2018/3/11.
  */
+class MainMould(val context:Context){
+
+    private fun requestSmilar(result:ClientResponse<LastFmBean>) {
+        NetService.requestNet({ service ->
+            service.requestSimilar(context.getString(R.string.coldplayMbid))
+        },result)
+    }
+}
--- app/src/main/java/com/zuo/client/present/MainPresent.kt
+++ app/src/main/java/com/zuo/client/present/MainPresent.kt
@@ -1,11 +1,17 @@
 package com.zuo.client.present
 
+import android.content.Context
 import com.zuo.client.contract.MainContract
+import com.zuo.client.mould.MainMould
 
 /**
  * Created by dongdong on 2018/3/11.
  */
-class MainPresent: MainContract.Present{
+class MainPresent(val context:Context): MainContract.Present{
+
+    init {
+        val mainMould = MainMould(context)
+    }
 
     override fun start() {
 
